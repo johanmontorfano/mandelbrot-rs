a1a563368bc81c7aeb92689d37ec414a2efc55c3
Johan Montorfano <me@johanmontorfano.com>
2024-02-01 19:20:29 +0100

    initial commit + working even if there's room for improvement

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+mod mandelbrot;
+
+use softbuffer::{Context, Surface};
+use std::num::NonZeroU32;
+use std::rc::Rc;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use crate::mandelbrot::Mandelbrot;
+
+const MAX_GEN_THREADS: u32 = 8;
+
+fn main() {
+    let event_loop = EventLoop::new().expect("Failed to initialize EventLoop");
+    let window = Rc::new(WindowBuilder::new().build(&event_loop).expect("Failed to initialize Window"));
+    let context = Context::new(window.clone()).expect("Failed to initialize window context.");
+    let mut surface = Surface::new(&context, window.clone()).expect("Failed to init surface.");
+
+    let mut scale = 0.4;
+    let mut x_additional_offset = 0.0;
+    let mut y_additional_offset = 0.0;
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    event_loop.run(move |event, window_target| {
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => { window_target.exit(); }
+            Event::WindowEvent { event: WindowEvent::RedrawRequested, .. } => {
+                let (width, height) = window.inner_size().into();
+
+                surface.resize(NonZeroU32::new(width).unwrap(), NonZeroU32::new(height).unwrap())
+                    .expect("Failed to resize the surface.");
+
+                let mut mandelbrot = Mandelbrot::init_with_offset_and_scale_for_coords(
+                    (width, height), scale
+                );
+                mandelbrot.offset_x += x_additional_offset;
+                mandelbrot.offset_y += y_additional_offset;
+
+                // To guarantee quick rendering time, the Mandelbrot generation is shared between threads. Hence, a
+                // static `MAX_GEN_THREADS` number of threads is created and sectors of the window are divided and
+                // shared between every thread.
+                // The window is divided horizontally, and to ensure maximum efficiency, generation results are shared
+                // through a channel whose listener directly adds computed colors to the display buffer.
+
+                let (tx, rx): (Sender<(u32, u32)>, Receiver<(u32, u32)>) = mpsc::channel();
+                let thread_gen_width_res = width / MAX_GEN_THREADS;
+
+                for thread in 0..MAX_GEN_THREADS {
+                    let thread_tx = tx.clone();
+
+                    thread::spawn(move || {
+                        let width_offset = thread_gen_width_res * thread;
+
+                        for x in width_offset..(width_offset + thread_gen_width_res) {
+                            for y in 0..height {
+                                let color = mandelbrot.get_point_color_at_coords(
+                                    x.clone() as i32, y.clone() as i32
+                                );
+                                let pixel_number = y * width + x;
+                                let bor_pixel = color[0] as u32 | (color[1] as u32) << 8 | (color[2] as u32) << 16;
+
+                                thread_tx.send((bor_pixel, pixel_number)).unwrap();
+                            }
+                        }
+                    });
+                }
+
+                let mut buffer = surface.buffer_mut().unwrap();
+                for _ in 0..(width * height) {
+                    let received_color = rx.recv().unwrap();
+                    buffer[received_color.1 as usize] = received_color.0;
+                }
+
+                buffer.present().unwrap();
+
+                println!("Rendered {} pixels with Mandelbrot.", width * height);
+            }
+            _ => ()
+        }
+    }).expect("Event loop listener failed.");
+}
---
+++ src/mandelbrot.rs
@@ -0,0 +1,51 @@
+use num::Complex;
+
+const MAX_ITER: u16 = 250;
+const PRECISION: f64 = 16.0;
+
+#[derive(Copy, Clone)]
+pub struct Mandelbrot {
+    width: f64,
+    height: f64,
+    pub offset_x: f64,
+    pub offset_y: f64,
+    scale: f64
+}
+
+impl Mandelbrot {
+    pub fn init_with_offset_and_scale_for_coords(size: (u32, u32), scale: f64) -> Self {
+        let width = size.0 as f64;
+        let height = size.1 as f64;
+
+        Self {
+            width, height, scale,
+            offset_x: width * 0.75,
+            offset_y: height / 2.0
+        }
+    }
+
+    // Computes a point color using Mandelbrot proper computations and the final result at allowed
+    // MAX_ITER.
+    pub fn get_point_color_at_coords(&self, x: i32, y: i32) -> [u8; 3] {
+        let x = (x as f64 - self.offset_x) / self.scale;
+        let y = (y as f64 - self.offset_y) / self.scale;
+
+        let c = Complex::new(x / self.width, y / self.height);
+        let mut z = Complex::new(0.0, 0.0);
+        let mut n = 0_u16;
+
+        while z.norm() < PRECISION && n < MAX_ITER {
+            z = z * z + c;
+            n += 1;
+        }
+
+        if n == MAX_ITER { [0, 0, 0] }
+        else {
+            let r = (n as f64 / MAX_ITER as f64 * 255.0) as u8;
+            let g = 0;
+            let b = 255 - r;
+
+            [r, g, b]
+        }
+    }
+}
